61fab7f6ea93e6bac795552ab28078148c759b34
Mitsuharu Uemoto <uemoto@ccs.tsukuba.ac.jp>
2018-05-18 09:46:26 +0900

    Feat: Modified MediaWiki Query Class

--- mw2doc.py
+++ mw2doc.py
@@ -3,43 +3,69 @@
 import urllib.request
 import urllib.parse
 import json
+import re
 
 
 class MediaWikiAPI:
 
     def __init__(self, wikiroot):
-        self.api_php = urllib.response.urljoin(wikiroot, "api.php")
-        self.index_php = urllib.response.urljoin(wikiroot, "index.php")
+        self.api_php = urllib.request.urljoin(wikiroot, "api.php")
+        self.index_php = urllib.request.urljoin(wikiroot, "index.php")
+    
+    def _escaped_title(self, title):
+        return '_'.join([
+            item.capitalize() for item in re.split(r"\s+", title)
+        ])
 
     def _call_api(self, **kwargs):
         api_args = urllib.parse.urlencode(kwargs).encode('utf-8')
-        request = urllib.request.Request(self.api_addr, api_args)
+        request = urllib.request.Request(self.api_php, api_args)
         with urllib.request.urlopen(request) as response:
             if response.getcode() != 200:
-                raise ValueError('Fail to connect "%s"' % self.api_addr)
+                raise ValueError('Fail to connect "%s"' % self.api_php)
             data = response.read()
         return data
-
-    def get_content(self, title):
+    
+    def get_pageid(self, title_list):
+        data = json.loads(self._call_api(
+            action='query', format='json',
+            titles='|'.join(title_list)
+        ))
+        normalized_table = {
+            item['from']: item['to'] 
+            for item in data['query'].get('normalized', {})
+        }
+        pageid_table = {
+            item['title']: pageid
+            for pageid, item in data['query']['pages'].items()
+        }
+        result = [
+            pageid_table[normalized_table.get(title, title)]
+            for title in title_list
+        ]
+        return result
+        
+    def get_contents(self, pageid_list):
+        # Call MediaWiki API
+        result = []
         data = json.loads(self._call_api(
             action='query', format='json',
             prop='revisions', rvprop='content',
-            titles=title
+            pageids='|'.join(pageid_list)
         ))
         pages = data['query']['pages']
-        page = pages[pages.keys()[0]]
-        title = page['title']
-        if 'missing' in page:
-            raise ValueError('Page "%s" is not found' % title)
-        for revision in page['revisions']:
-            content = revision['*']
-        return title, content
-
+        for pageid in pageid_list:
+            if 'missing' in pages[pageid]:
+                raise ValueError('PageID "%s" is not found' % pageid)
+            title = pages[pageid]['title']
+            content = pages[pageid]['revisions'][-1]['*']
+            result += [(title, content)]
+        return result
+        
     def get_images(self, title):
         data = json.loads(self._call_api(
             action='query', format='json',
-            prop='revisions', rvprop='content',
-            titles=title
+            prop='images', titles=title
         ))
         pages = data['query']['pages']
         page = pages[pages.keys()[0]]
@@ -49,14 +75,14 @@ class MediaWikiAPI:
         for revision in page['revisions']:
             content = revision['*']
         return title, content
-    
+
     def save_uploaded_file(self, title, filename):
         special_page = "%s?Special:Filepath/%s" % (self.index_php, title)
         with urllib.request.urlopen(special_page) as response:
             with open(filename, "wb") as fh:
                 fh.write(response.read())
-    
 
+#api.php?action=query&titles=File:Albert%20Einstein%20Head.jpg&prop=imageinfo&&iiprop=ur
 
 class Document:
 
